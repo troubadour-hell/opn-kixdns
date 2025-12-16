31e98c7a2fdd27d60e7700e670c26f2f99328c14
troubadour-hell <17946453+troubadour-hell@users.noreply.github.com>
2025-12-16 18:26:32 +0800

    Add KixDNS model and forms for pipeline, rule, selector, and general settings

---
+++ src/opnsense/mvc/app/models/OPNsense/KixDNS/KixDNS.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace OPNsense\KixDNS;
+
+use OPNsense\Base\BaseModel;
+
+/**
+ * KixDNS model
+ *
+ * This class is backed by the XML definition in KixDNS.xml
+ * and is instantiated via ApiMutableModelControllerBase.
+ */
+class KixDNS extends BaseModel
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
+
+
